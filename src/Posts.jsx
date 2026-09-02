a77617cc275f3209405f804f0c1279cc0f79258a
anas18x <anasshakil00@gmail.com>
2026-09-02 18:38:44 +0530

    pagination

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -1,26 +1,33 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import axios from "axios";
 import {Link, useParams} from "react-router-dom";
+import { useState } from "react";
+
 
 
 // React Query doesn't look for return error; it looks at whether your queryFn Promise resolves or rejects.
-async function getPost(){
-    const data = await axios.get("https://jsonplaceholder.typicode.com/posts");
+async function getPost(pageNumber){
+    const data = await axios.get(`https://jsonplaceholder.typicode.com/posts?_start=${pageNumber}&_limit=3`);
     return data;
 }
 
 
 export function Posts() {
-  const { data, isError, error, isLoading } = useQuery({
-    queryKey: ["posts"],
-    queryFn: getPost,
-    staleTime: 5 * 60 * 1000  // 5 minutes
+  const [pageNumber, setPageNumber] = useState(0 );
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ["posts",pageNumber],
+    queryFn: () => getPost(pageNumber),
+    staleTime: 5 * 60 * 1000,  // 5 minutes
+    placeholderData: keepPreviousData, // This option allows React Query to use the previous data as a placeholder while fetching new data, providing a smoother user experience.
   });
+f
 
   if(isLoading) return <p>Loading ...</p>
   if(isError) return <p>Something went wrong</p>
 
   return (
+    <>
     <div style={{ display: "grid", gridTemplateColumns: "repeat(auto-fill, minmax(300px, 1fr))", gap: "16px", padding: "16px" }}>
 
       {data?.data?.map((post) => (
@@ -31,16 +38,30 @@ export function Posts() {
           style={{
             padding: "16px", border: "1px solid #ddd",borderRadius: "8px", boxShadow: "0 2px 6px rgba(0,0,0,0.08)",
           }}
-        >
+        >  
+          <h2>{post.id}</h2>
           <h2 style={{ margin: "0 0 8px" }}>{post.title}</h2>
           <p style={{ margin: 0, color: "#666" }}>{post.body}</p>
         </div>
         </Link>
-      ))}
+      ))} 
 
     </div>
+
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "16px", marginTop: "16px" }}>
+    
+      <button disabled={pageNumber === 0 ? true : false}
+      style={{ padding: "8px 16px", border: "1px solid #ccc", backgroundColor: "transparent", cursor: "pointer" ,borderRadius: "4px" }} onClick={() => setPageNumber(pageNumber - 3)}>Prev</button>
+
+      <h2 style={{textAlign: "center",}}> {pageNumber} </h2>
+
+      <button style={{ padding: "8px 16px", border: "1px solid #ccc", backgroundColor: "transparent", cursor: "pointer" ,borderRadius: "4px" }} onClick={() => setPageNumber(pageNumber + 3)}>Next</button>
+    </div>
+    </>
   );
 }
+
+
 
 
 export function PostComponent() {
